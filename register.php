7b5ac153860ad7b84f7ebb10cd52a11f17dc96fa
Tyman Sin <sin.tyman@yahoo.com>
2018-01-13 14:04:23 -0700

    added more registration fields

--- register.php
+++ register.php
@@ -14,16 +14,28 @@
 
     </div>
     <div id="form">
-      <form>
-        Name:<br>
+      <form action="/index.php">
+        <label>Name:</label>
         <input type="text" name="name">
         <br>
-        Height:<br>
+
+        <label>Height:</label>
         <input type="number" name="feet"> feet and <input type="number" name="inches"> inches
         <br>
-        Weight:<br>
-        <input type="number" name="weight"> lbs
 
+        <label>Weight:</label>
+        <input type="number" name="weight"> lbs
+        <br>
+
+        <label>Goal:</label><br>
+        <select name="goals" size="3" multiple>
+          <option value="weightloss">Lose Weight</option>
+          <option value="musclegain">Gain Muscle</option>
+          <option value="endurancegain">Improve Aerobic Endurance</option>
+        </select>
+        <br>
+
+        <input type="submit" value="Submit">
       </form>
     </div>
   </body>
