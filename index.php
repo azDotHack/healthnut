2908209534ca6ab8ffc81e240a7d258f96739365
BenjaminLadick <bladick@asu.edu>
2018-01-13 11:08:23 -0700

    Began the home page. Got some images going.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Healthy Habits</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="jquery-3.2.1.js"></script>
+  <script>
+
+  </script>
+</head>
+<body>
+  <div id="head">
+
+  </div>
+  <div id="img_slide">
+    <figure>
+      <img class="img_frame" src="stock_img0.jpg" alt="Even you can start healthy habits">
+      <img class="img_frame" src="stock_img1.jpg" alt="This can be changed later">
+      <img class="img_frame" src="stock_img2.jpg" alt="This can be changed later"> <!-- TODO change these alts -->
+      <img class="img_frame" src="stock_img3.jpg" alt="This can be changed later">
+    </figure>
+  </div>
+  <div id="foot">
+
+  </div>
+</body>
+</html>
