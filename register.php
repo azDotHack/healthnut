26e72b75a67061fdf0285bcc47388947644048ce
Tyman Sin <sin.tyman@yahoo.com>
2018-01-13 12:28:09 -0700

    Added registration page
    register.php added with some forms included

---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Healthy Habits</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="jquery-3.2.1.js"></script>
+    <script>
+
+    </script>
+  </head>
+  <body>
+    <div id="head">
+
+    </div>
+    <div id="form">
+      <form>
+        Name:<br>
+        <input type="text" name="name">
+        <br>
+        Height:<br>
+        <input type="number" name="feet"> feet and <input type="number" name="inches"> inches
+        <br>
+        Weight:<br>
+        <input type="number" name="weight"> lbs
+
+      </form>
+    </div>
+  </body>
