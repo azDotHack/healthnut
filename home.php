0059baaf4262e3d00869e13db20b7a2c59dc018e
Tyman Sin <sin.tyman@yahoo.com>
2018-01-13 21:52:21 -0700

    Changed CSS, added flexbox layout and more dummy data

--- home.php
+++ home.php
@@ -1,8 +1,7 @@
 <link rel="stylesheet" href="style.css">
 
 <style>
-table.mytask {
-    width: 40%;
+.mytask, .motivatortask {
     border-collapse: collapse;
 }
 
@@ -19,8 +18,17 @@ td.complete{
   text-align: center;
 }
 
-div.sidebar{
-  float:right;
+.flex-container{
+  display: flex;
+}
+
+.flex-container>div{
+  margin: 20px;
+}
+
+.section{
+  margin:10px;
+  padding:20px;
 }
 </style>
 
@@ -32,119 +40,130 @@ div.sidebar{
   <img id="profile" src="" alt="Profile">
 </div>
 
-<div>
-<div>
-  <div id="tasks">
-    <header>
-      <h3>My Tasks/Habits</h3> <!-- TODO change this text -->
-    </header>
+<div class="flex-container">
+  <div id="west" style="flex-grow: 6">
+    <div id="tasks" class="section">
+      <header>
+        <h3>My Tasks/Habits</h3> <!-- TODO change this text -->
+      </header>
 
-    <div id="tasklist">
+      <div id="tasklist">
 
-      <!--TODO implement PHP code to generate this HTML-->
-      <table class="mytask">
-        <tr>
-          <th>Daily</th>
-          <th>Due: 1/14/2018</th>
-        </tr>
-        <tr>
-          <td class="task">Do 10 Jumping Jacks</td>
-          <td class="complete">
-            <form>
-              <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
-            </form>
-          </td>
-        </tr>
-      </table>
-      <br>
+        <!-- TODO implement PHP code to generate this HTML -->
+        <table class="mytask">
+          <tr>
+            <th>Daily</th>
+            <th>Due: 1/14/2018</th>
+          </tr>
+          <tr>
+            <td class="task">Do 10 Jumping Jacks</td>
+            <td class="complete">
+              <form>
+                <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
+              </form>
+            </td>
+          </tr>
+        </table>
+        <br>
 
-      <table class="mytask">
-        <tr>
-          <th>Daily</th>
-          <th>Due: 1/14/2018</th>
-        </tr>
-        <tr>
-          <td class="task">Do 10 Lunges</td>
-          <td class="complete">
-            <form>
-              <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
-            </form>
-          </td>
-        </tr>
-      </table>
-      <br>
+        <table class="mytask">
+          <tr>
+            <th>Daily</th>
+            <th>Due: 1/14/2018</th>
+          </tr>
+          <tr>
+            <td class="task">Do 10 Lunges</td>
+            <td class="complete">
+              <form>
+                <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
+              </form>
+            </td>
+          </tr>
+        </table>
+        <br>
 
-      <table class="mytask">
-        <tr>
-          <th>Weekly: At the Gym</th>
-          <th>Due: 1/19/2018</th>
-        </tr>
-        <tr>
-          <td class="task">Run 5km on the Treadmill</td>
-          <td class="complete">
-            <form>
-              <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
-            </form>
-          </td>
-        </tr>
-        <tr>
-          <td class="task">Bench Press: 5 sets of 5 reps</td>
-          <td class="complete">
-            <form>
-              <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
-            </form>
-          </td>
-        </tr>
-        <tr>
-          <td class="task">Swim 200m</td>
-          <td class="complete">
-            <form>
-              <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
-            </form>
-          </td>
-        </tr>
-      </table>
-      <!-- end of PHP code generation -->
+        <table class="mytask">
+          <tr>
+            <th>Weekly: At the Gym</th>
+            <th>Due: 1/19/2018</th>
+          </tr>
+          <tr>
+            <td class="task">Run 5km on the Treadmill</td>
+            <td class="complete">
+              <form>
+                <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
+              </form>
+            </td>
+          </tr>
+          <tr>
+            <td class="task">Bench Press: 5 sets of 5 reps</td>
+            <td class="complete">
+              <form>
+                <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
+              </form>
+            </td>
+          </tr>
+          <tr>
+            <td class="task">Swim 200m</td>
+            <td class="complete">
+              <form>
+                <button type="button" onclick="alert('Conglaturation! Your winner')">Complete!</button>
+              </form>
+            </td>
+          </tr>
+        </table>
+        <br>
+        <!-- end of PHP code generation -->
+      </div>
+
+      <footer>
+          <a href="tasks.php">More Details</a> <!-- TODO link this correctly -->
+      </footer>
     </div>
 
-    <footer>
-        <a href="tasks.php">More Details</a> <!--TODO link this correctly-->
-    </footer>
+    <div id="motivators" class="section">
+      <header>
+        <h3>Your Motivators</h3>
+      </header>
+    </div>
   </div>
 
-  <div id="motivators">
-    <header>
-      <h3>Your Motivators</h3>
-    </header>
+  <div id="east" style="flex-grow: 4">
+    <div id="social" class="section">
+      <header>
+        <h3>Social</h3>
+      </header>
+
+      <div id="verifylist">
+        <!-- TODO implement PHP to generate this HTML -->
+        <table class="motivatortask">
+          <tr>
+            <th rowspan="3"><img id="motivatorpfp" src="" alt="James Rund"></th>
+            <th>James Rund</th>
+            <th>Verify:</th>
+          </tr>
+          <tr>
+            <td class="task">Fall Asleep at Hackathon</td>
+              <td class="complete">
+                <form>
+                  <button type="button" onclick="alert('Conflagration! They winned')">Complete!</button>
+                </form>
+              </td>
+          </tr>
+          <tr>
+            <td class="task">Pitch Hackathon idea</td>
+              <td class="complete">
+                Incomplete
+              </td>
+          </tr>
+        </table>
+        <br>
+        <!-- end of PHP code generation -->
+      </div>
+
+      <footer>
+        <a href="social.php">See more</a> <!--TODO link this correctly-->
+      </footer>
+    </div>
   </div>
-</div>
-
-<div id="social" class="sidebar">
-  <header>
-    <h3>Social</h3>
-  </header>
-
-  <div id="verifylist">
-    <table>
-      <tr>
-        <th rowspan="2"><img id="motivatorpfp" src="" alt="James Rund"></th>
-        <th>James Rund</th>
-        <th>Verify:</th>
-      </tr>
-      <tr>
-        <td class="task">Fall Asleep at Hackathon</th>
-        <td class="complete">
-          <form>
-            <button type="button" onclick="alert('Conflagration! They winned')">Complete!</button>
-          </form>
-        </td>
-      </tr>
-    </table>
-    <br>
-  </div>
-
-  <footer>
-    <a href="social.php">See more</a> <!--TODO link this correctly-->
-  </footer>
-</div>
 </div>
